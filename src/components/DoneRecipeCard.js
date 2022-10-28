436f239a498ff8660cb566b6efa2425f0c84792b
menezesrenan <renancarmomenezes@gmail.com>
2022-10-28 16:21:24 -0300

    Requisitos DoneRecipes

--- src/components/DoneRecipeCard.js
+++ src/components/DoneRecipeCard.js
@@ -1,84 +1,164 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import ShareIcon from '../images/shareIcon.svg';
+import { useHistory, Link } from 'react-router-dom';
+import ShareButton from './ShareButton';
 
-function DoneRecipeCard({ recipe, index }) {
-  const pathToRecipeDetails = recipe.meals ? (
-    `/meals/${recipe.idMeal}`) : `/drinks/${recipe.idDrink}`;
-  console.log(recipe);
-  return (
-    <div>
-      <Link to={ pathToRecipeDetails }>
-        <img
-          key={ index }
-          height="200px"
-          width="200px"
-          data-testid={ `${index}-horizontal-image` }
-          src={ recipe.strDrinkThumb || recipe.strMealThumb }
-          alt="meals"
-        />
-      </Link>
-      <div>
-        <div>
-          <Link to={ pathToRecipeDetails }>
-            <h1 data-testid={ `${index}-horizontal-name` }>
-              {recipe.strMeal || recipe.strDrink}
-            </h1>
-          </Link>
-          <p data-testid={ `${index}-horizontal-top-text` }>
-            { recipe.meals ? (
-              `${recipe.strArea} ${recipe.strCategory}`
-            ) : recipe.strAlcoholic }
-          </p>
-        </div>
-        <div>
+function DoneRecipeCard({ food }) => {
+const history = useHistory();
+
+const redirectToDetails = ({ id, type }) => {
+  if (type === 'meal') {
+    history.push(`/meals/${id}`);
+  } else {
+    history.push(`/drinks/${id}`);
+  }
+};
+
+return(
+    meals.map((receita, index) => (
+      <div
+        key={ Math.random() }
+      >
+
+        <Link to={ receita.type === 'meal' ? `/meals/${receita.id}` : `/drinks/${receita.id}` }>
+          <h3
+            data-testid={ `${index}-horizontal-name` }
+          >
+            { receita.name }
+          </h3>
+        </Link>
+
+        {/* <Link to={ receita.type === 'meal' ? `/meals/${receita.id}` : `/drinks/${receita.id}` }> */}
+          <img
+            src={ receita.image }
+            alt="Imagem da receita"
+            data-testid={ `${index}-horizontal-image` }
+            onClick={ () => redirectToDetails }
+          />
+
+        {/* </Link> */}
+        <div className="done-recipe-describe">
+          <div className="done-recipe-top">
+            <p
+              data-testid={ `${index}-horizontal-top-text` }
+            >
+              { receita.alcoholicOrNot === ''
+                ? `${receita.nationality} - ${receita.category}`
+                : receita.alcoholicOrNot }
+            </p>
+
+            <ShareButton
+              dataId={ `${index}-horizontal-share-btn` }
+              URL={ receita.type === 'meal' ? `/meals/${receita.id}` : `/drinks/${receita.id}` }
+            />
+          </div>
+
           <p
             data-testid={ `${index}-horizontal-done-date` }
           >
-            {recipe.doneDate}
-            {/* doneDate data que terminou a */}
+            { receita.doneDate }
           </p>
-        </div>
-        <div>
-          <p
-            data-testid={ `${index}-horizontal-share-btn` }
-            img
-            src={ ShareIcon }
-          >
-            Compartilhar receita
-          </p>
-          {/* Preciso instalar um import clipboard */}
-        </div>
-        <div>
-          { recipe.tags && recipe.tags.map((tagName) => (
-            <div
-              key={ tagName }
-              data-testid={ `${index}-${tagName}-horizontal-tag` }
-            >
-              {tag}
-            </div>
-          ))}
+
+          <div>
+            {
+              receita.tags.map((tag) => (
+                <p
+                  key={ tag }
+                  data-testid={ `${index}-${tag}-horizontal-tag` }
+                >
+                  { tag }
+                </p>
+              ))
+            }
+          </div>
         </div>
       </div>
-
-    </div>
+    ))
   );
 }
 
-DoneRecipeCard.propTypes = {
-  recipe: PropTypes.shape({
-    alcoholicOrNot: PropTypes.string,
-    category: PropTypes.string,
-    doneDate: PropTypes.string,
-    id: PropTypes.string,
-    image: PropTypes.string,
-    name: PropTypes.string,
-    nationality: PropTypes.string,
-    tags: PropTypes.arrayOf(PropTypes.string),
-    type: PropTypes.string,
-  }).isRequired,
-  index: PropTypes.number.isRequired,
-};
-
 export default DoneRecipeCard;
+
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { Link } from 'react-router-dom';
+// import ShareIcon from '../images/shareIcon.svg';
+
+// function DoneRecipeCard({ recipe, index }) {
+//   const pathToRecipeDetails = recipe.meals ? (
+//     `/meals/${recipe.idMeal}`) : `/drinks/${recipe.idDrink}`;
+//   console.log(recipe);
+//   return (
+//     <div>
+//       <Link to={ pathToRecipeDetails }>
+//         <img
+//           key={ index }
+//           height="200px"
+//           width="200px"
+//           data-testid={ `${index}-horizontal-image` }
+//           src={ recipe.strDrinkThumb || recipe.strMealThumb }
+//           alt="meals"
+//         />
+//       </Link>
+//       <div>
+//         <div>
+//           <Link to={ pathToRecipeDetails }>
+//             <h1 data-testid={ `${index}-horizontal-name` }>
+//               {recipe.strMeal || recipe.strDrink}
+//             </h1>
+//           </Link>
+//           <p data-testid={ `${index}-horizontal-top-text` }>
+//             { recipe.meals ? (
+//               `${recipe.strArea} ${recipe.strCategory}`
+//             ) : recipe.strAlcoholic }
+//           </p>
+//         </div>
+//         <div>
+//           <p
+//             data-testid={ `${index}-horizontal-done-date` }
+//           >
+//             {recipe.doneDate}
+//             {/* doneDate data que terminou a */}
+//           </p>
+//         </div>
+//         <div>
+//           <p
+//             data-testid={ `${index}-horizontal-share-btn` }
+//             img
+//             src={ ShareIcon }
+//           >
+//             Compartilhar receita
+//           </p>
+//           {/* Preciso instalar um import clipboard */}
+//         </div>
+//         <div>
+//           { recipe.tags && recipe.tags.map((tagName) => (
+//             <div
+//               key={ tagName }
+//               data-testid={ `${index}-${tagName}-horizontal-tag` }
+//             >
+//               {tag}
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// DoneRecipeCard.propTypes = {
+//   recipe: PropTypes.shape({
+//     alcoholicOrNot: PropTypes.string,
+//     category: PropTypes.string,
+//     doneDate: PropTypes.string,
+//     id: PropTypes.string,
+//     image: PropTypes.string,
+//     name: PropTypes.string,
+//     nationality: PropTypes.string,
+//     tags: PropTypes.arrayOf(PropTypes.string),
+//     type: PropTypes.string,
+//   }).isRequired,
+//   index: PropTypes.number.isRequired,
+// };
+
+// export default DoneRecipeCard;
--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,12 +1,21 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import DoneRecipeCard from '../components/DoneRecipeCard';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import oneDrinks from '../helpers/MockDrinks';
 
 export default function DoneRecipes() {
-  const [doneRecipes, setDoneRecipes] = useState([]);
-  const [activeFilter, setFilter] = useState('');
+  const [filter, setFilter] = useState('all');
+  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
+
+  // console.log(doneRecipes);
+
+  const renderRecipes = filter === 'all'
+    ? doneRecipes
+    : doneRecipes.filter((meal) => meal.type === filter);
+  // const [doneRecipes, setDoneRecipes] = useState([]);
+  // const [activeFilter, setFilter] = useState('');
 
   const key = 'doneRecipes';
   const mockData = oneDrinks;
@@ -17,9 +26,9 @@ export default function DoneRecipes() {
 
   // console.log(doneRecipes);
 
-  useEffect(() => {
-    setDoneRecipes(JSON.parse(localStorage.getItem('doneRecipes')));
-  }, []);
+  // useEffect(() => {
+  //   setDoneRecipes(JSON.parse(localStorage.getItem('doneRecipes')));
+  // }, []);
 
   // console.log(doneRecipes[0]['drinks']);
 
@@ -31,7 +40,7 @@ export default function DoneRecipes() {
           type="button"
           data-testid="filter-by-all-btn"
           onClick={ () => {
-            setFilter('');
+            setFilter('all');
           } }
         >
           All
@@ -55,12 +64,26 @@ export default function DoneRecipes() {
           Drinks
         </button>
       </div>
-      {/* {console.log(doneRecipes[0]['drinks'])} */}
-      {doneRecipes.length !== 0 && doneRecipes[0]['drinks']
+      <section>
+        { renderRecipes.length === 0
+          ? (
+            <div>
+              <p>Sem receitas finalizadas</p>
+            </div>
+          )
+          : (
+            <DoneRecipeCard
+              food={ renderRecipes }
+            />
+          ) }
+      </section>
+
+      {/* {console.log(doneRecipes[0]['drinks'])}
+      {doneRecipes.length === 0 && doneRecipes[0]['drinks']
         // .filter((recipe) => recipe.type.includes(activeFilter))
         .map((recipe, index) => (
           <DoneRecipeCard key={ recipe.idDrink } recipe={ recipe } index={ index } />
-        )) }
+        )) } */}
 
       <Footer />
     </div>
